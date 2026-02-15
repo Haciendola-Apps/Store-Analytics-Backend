43b0189baf78ed42a968ea697057b301168c1749
Felipe Sanmartin <fel.e.sanmartin@gmail.com>
2026-02-14 22:11:03 -0300

    test2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
+
+  logger.log(`APPLICATION INICIADA EN EL PUERTO ${port}`);
+  logger.log(`HEALTH CHECK DISPONIBLE EN: http://localhost:${port}/`);
 }
+
 bootstrap();
