ec729c708a1d27203614917e5384e4d5ed7c71d4
Felipe Sanmartin <fel.e.sanmartin@gmail.com>
2026-02-17 04:49:39 -0300

    another fix to main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,15 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-
+  
+  app.setGlobalPrefix('api'); 
+  
   app.enableCors();
 
   const port = process.env.PORT || process.env.NODE_PORT || 3000;
-
   await app.listen(port, '0.0.0.0');
 
   logger.log(`APPLICATION INICIADA EN EL PUERTO ${port}`);
-  logger.log(`HEALTH CHECK DISPONIBLE EN: http://localhost:${port}/`);
 }
 
 bootstrap();
